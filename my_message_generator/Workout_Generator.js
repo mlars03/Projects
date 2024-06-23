7055522fe9e52be6696d2e26b97d6a755f997721
Matt Larson <sf49ersgiants@gmail.com>
2024-06-23 17:23:29 -0500

    first commit, added the arrays of exercises and quotes that will be randomly pulled

---
+++ my_message_generator/Workout_Generator.js
@@ -0,0 +1,43 @@
+// Program will generate a randomized workout routine and motivational quote.
+
+const cardio = ['stationary bike', 'treadmill', 'eliptical', 'rowing machine', 'jump rope', 'stair climber'];
+const upperBody = [
+    'push-ups',
+    'pull-ups',
+    'dumbbell curls',
+    'shoulder press',
+    'bench press',
+    'tricep dips',
+    'bicep curls',
+    'chest flyes'
+];
+const lowerBody = [
+    'squats',
+    'lunges',
+    'deadlifts',
+    'leg press',
+    'calf raises',
+    'hamstring curls',
+    'leg extensions',
+    'glute bridges'
+];
+const core =[
+    'plank',
+    'sit-ups',
+    'bicycle crunches',
+    'mountain climbers',
+    'Russian twists',
+    'leg raises',
+    'flutter kicks',
+    'side plank'
+];
+const quote = [
+    "Don't watch the clock; do what it does. Keep going.",
+    "Success is what comes after your stop making excuses.",
+    "The harder you work for something, the greater you'll feel when you achieve it.",
+    "Dream it. Wish it. Do it.",
+    "Success doesn’t just find you. You have to go out and get it.",
+    "Push yourself because no one else is going to do it for you.",
+    "Great things never come from comfort zones.",
+    "Dream bigger. Do bigger."
+];
